efe7776bab5cd28a9d70f03f8fc1bd704ae84be2
Joseph Michael Corrado <joeyycorrss@gmail.com>
2021-11-05 18:31:24 -0400

    finshed my solution for progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,57 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  large: {
+    wrapper: {
+      '--padding': `${4/16}rem`,
+      '--border-radius': '8px',
+    },
+    valueBar: {
+      '--padding-block': '8px',
+    },
+  },
+  medium: {
+    wrapper: {
+      '--padding': `${0/16}rem`,
+      '--border-radius': '4px',
+    },
+    valueBar: {
+      '--padding-block': '6px',
+    },
+  },
+  small: {
+    wrapper: {
+      '--padding': `${0/16}rem`,
+      '--border-radius': '4px',
+    },
+    valueBar: {
+      '--padding-block': '4px',
+    },
+  },
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size]
+  return (
+  <Wrapper style={styles.wrapper} aria-valuenow={value} aria-valuemin={0} aria-valuemax={100}>
+    <ValueBar style={styles.valueBar} value={value}/>
+    <VisuallyHidden>Progress Bar</VisuallyHidden>
+  </Wrapper>
+  );
 };
+
+const ValueBar = styled.div`
+  background-color: ${COLORS.primary};
+  padding-block: var(--padding-block);
+  border-radius: ${p => p.value > 99 ? '4px' : '4px 0 0 4px'};
+  margin-right: ${p => (p.value < 100 && 100 - p.value)}%;
+`
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--border-radius);
+  padding: var(--padding);
+`
 
 export default ProgressBar;
