7db43e552aa3ffe5a8e633b720ce6aac30c6dd31
Joseph Michael Corrado <joeyycorrss@gmail.com>
2021-11-06 15:33:58 -0400

    mixed his solution with a tiny bit of mine [Select]

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,53 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <PaddedIcon id="chevron-down" strokeWidth={1} size={24}/>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  appearance: none;
+  -webkit-appearance: none;
+`
+const PaddedIcon = styled(Icon)`
+  margin-inline-start: 14px;
+  ${'' /* pointer-events: none; */}
+`
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  font-size: ${16/16}rem;
+  display: flexbox;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 8px;
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+    outline: auto;
+  }
+  ${NativeSelect}:hover + & {
+    color: black;
+  }
+`
 
 export default Select;
